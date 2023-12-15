f36796f9641f3258c4018ac0ab614ca4254cf0a9
Vasil Genov <vasil.genov@psinteractive.io>
2023-12-15 18:13:57 +0200

    feat: tutorial start

---
+++ _01pixi-tutorial/src/index.ts
@@ -0,0 +1,8 @@
+import { SceneManager } from './shared/scene-manager'
+import { LoaderScene } from './scenes/loader-scene'
+import { FILL_COLOR } from './shared/constants'
+
+SceneManager.init(FILL_COLOR)
+
+const loady: LoaderScene = new LoaderScene()
+SceneManager.changeScene(loady)
